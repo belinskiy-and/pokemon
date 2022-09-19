1ee4511303b12a3201328aca8c34315cde74bef9
belinskiy-and <90721066+belinskiy-and@users.noreply.github.com>
2022-09-19 19:38:37 +0300

    added pokemon info

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,52 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      Hello, React
-    </div>
-  );
+import { Component } from "react";
+import SearchForm from "./SearchForm";
+
+const Status = {
+  IDLE: "idle",
+  PENDING: "pending",
+  RESOLVED: "resolved",
+  REJECTED: "rejected"
+}
+
+export class App extends Component {
+
+  state = {
+    pokemonName: null,
+    pokemon: null,
+    isLoading: false,
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    const prevName = prevState.pokemonName;
+    const nextName = this.state.pokemonName;
+    
+    if (prevName !== nextName) {
+      
+      this.setState({ isLoading: true });
+      setTimeout( () =>
+        fetch(`https://pokeapi.co/api/v2/pokemon/${nextName}`)
+          .then(res => res.json())
+          .then(pokemon => this.setState({pokemon}))
+          .finally(() => this.setState({ isLoading: false })),
+        6000);
+    }
+  }
+
+  searchSubmit = (pokemonName) => {
+      this.setState({pokemonName});    
+  }
+
+  render() {
+    const { pokemonName, pokemon, isLoading } = this.state;
+
+    return (
+      <>
+        <SearchForm searchSubmit={this.searchSubmit} />
+
+        {!pokemonName && <h3>Enter pokemon name</h3>}
+        {isLoading && <h3>Loading pokemon...</h3>}
+        {pokemon && <h2>{pokemon.name}</h2>}
+      </>
+    );
+  }
 };
